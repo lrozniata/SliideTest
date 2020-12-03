bf17881913423af7327b9da058ab748459f1d866
Wulffmorgenthalerr <39669282+Wulffmorgenthalerr@users.noreply.github.com>
2020-12-03 21:59:36 +0100

    Getting ImageRefactor
    Method takeScreenshotFromScreen added

--- src/test/java/com/sliide/QA_ENGINEER_SLIIDE_TEST/NewsApp_LogIn_tests.java
+++ src/test/java/com/sliide/QA_ENGINEER_SLIIDE_TEST/NewsApp_LogIn_tests.java
@@ -2,24 +2,13 @@ package com.sliide.QA_ENGINEER_SLIIDE_TEST;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-import com.sliide.QA_ENGINEER_SLIIDE_TEST.DriverProvider.*;
-import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.interactions.internal.Coordinates;
-import org.openqa.selenium.remote.Augmenter;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
 
 import static com.sliide.QA_ENGINEER_SLIIDE_TEST.DriverProvider.*;
@@ -44,15 +33,22 @@ public class NewsApp_LogIn_tests {
     @Test
     public void dummy() throws IOException, InterruptedException {
         AndroidElement el = androidDriver.findElement(By.id("com.test.news:id/editTextUserName"));
-        String s = el.getAttribute("class");
+        //String s = el.getAttribute("class");
+
+
         el.sendKeys("Useeeeer");
+        File wrongCredaentiialImageBeforeLogin = Tools.takeScreenshotFromScreen(androidDriver,900,300,550,250, "BeforeLigin.png");
         AndroidElement loginButton= androidDriver.findElement(By.id("com.test.news:id/buttonLogin"));
         loginButton.click();
-   /*     Coordinates cor = loginButton.getCoordinates();
-        cor.inViewPort();*/
+
         Thread.sleep(5000);
 
-        WebDriver driver1 = new Augmenter().augment(androidDriver);
+        File wrongCredaentiialImageAfterLogin = Tools.takeScreenshotFromScreen(androidDriver,900,300,550,250, "AfterLigin.png");
+
+
+
+
+       /* WebDriver driver1 = new Augmenter().augment(androidDriver);
         File file = ((TakesScreenshot) driver1).getScreenshotAs(OutputType.FILE);
         File screenshot = new File("Screenshot.png");
         FileUtils.copyFile(file, screenshot);
@@ -60,13 +56,14 @@ public class NewsApp_LogIn_tests {
         BufferedImage bufferedImageCroped = Tools.cropImage(bufferedImage,900, 300, 550,250);
         File pathFile = new File("Screenshot_crop.png");
         ImageIO.write(bufferedImageCroped,"png", pathFile);
-        pathFile = new File("ScreenshotPattern.png");
         screenshot = new File("Screenshot_crop.png");
-        Boolean b = Tools.compareImage(pathFile,screenshot);
+        pathFile = new File("ScreenshotPattern.png");*/
+
+        Boolean b = Tools.compareImage(wrongCredaentiialImageBeforeLogin,wrongCredaentiialImageAfterLogin);
 
 
         System.out.println(b);
-       /* System.out.println( loginButton.getCoordinates().inViewPort().toString());;*/
+
 
 
 
--- src/test/java/com/sliide/QA_ENGINEER_SLIIDE_TEST/Tools.java
+++ src/test/java/com/sliide/QA_ENGINEER_SLIIDE_TEST/Tools.java
@@ -1,9 +1,19 @@
 package com.sliide.QA_ENGINEER_SLIIDE_TEST;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.Augmenter;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBuffer;
 import java.io.File;
+import java.io.IOException;
 
 public class Tools {
     /**
@@ -46,6 +56,21 @@ public class Tools {
             System.out.println("Failed to compare image files ...");
             return  false;
         }
+    }
+
+
+   public static File takeScreenshotFromScreen(AndroidDriver androidDriver , int x, int y, int width, int height, String ScreenShotName) throws IOException, InterruptedException {
+
+        WebDriver driver1 = (new Augmenter()).augment(androidDriver);
+        File file = (File)((TakesScreenshot)driver1).getScreenshotAs(OutputType.FILE);
+        File screenshot = new File("Screenshot.png");
+        FileUtils.copyFile(file, screenshot);
+        BufferedImage bufferedImage = ImageIO.read(screenshot);
+        BufferedImage bufferedImageCroped = Tools.cropImage(bufferedImage,x,y, width, height);
+       File croppedScreenshot = new File(ScreenShotName);
+       ImageIO.write(bufferedImageCroped,"png", croppedScreenshot);
+
+        return croppedScreenshot;
     }
 
 
